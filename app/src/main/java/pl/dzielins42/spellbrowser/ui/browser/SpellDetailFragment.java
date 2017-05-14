7f5c5b25c486276cdbb94acbef809cc4e1b79ea5
Dominik Zieliński <dzielins42@gmail.com>
2017-05-14 23:36:56 +0200

    Improve spell details screen
    - Hide empty sections
    - Show schools, subschools (if applicable) and descriptors (if applicable)
    - Show sources/levels
    - show components

--- app/src/main/java/pl/dzielins42/spellbrowser/ui/browser/SpellDetailFragment.java
+++ app/src/main/java/pl/dzielins42/spellbrowser/ui/browser/SpellDetailFragment.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -15,13 +21,19 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import pl.dzielins42.masterdetailflowrevised.AbsMasterDetailActivity;
 import pl.dzielins42.spellbrowser.R;
+import pl.dzielins42.spellbrowser.data.model.SourceLevelExtraTriplet;
 import pl.dzielins42.spellbrowser.data.model.Spell;
+import pl.dzielins42.toolkit.android.widget.CaptionLayout;
 
-public class SpellDetailFragment extends Fragment implements Browser.DetailView{
+public class SpellDetailFragment extends Fragment implements Browser.DetailView {
+
+    private static final String TAG = SpellDetailFragment.class.getSimpleName();
 
     @Inject
     SpellDetailPresenter mPresenter;
 
+    @BindView(R.id.container_spell_levels)
+    ViewGroup mContainerLevels;
     @BindView(R.id.tv_spell_name)
     TextView mTVName;
     @BindView(R.id.tv_spell_schools)
@@ -32,22 +44,40 @@ public class SpellDetailFragment extends Fragment implements Browser.DetailView{
     TextView mTVDescriptors;
     @BindView(R.id.tv_spell_levels)
     TextView mTVLevels;
+    @BindView(R.id.cl_spell_levels)
+    CaptionLayout mCLLevels;
     @BindView(R.id.tv_spell_components)
     TextView mTVComponents;
+    @BindView(R.id.cl_spell_components)
+    CaptionLayout mCLComponents;
     @BindView(R.id.tv_spell_casting_time)
     TextView mTVCastingTime;
+    @BindView(R.id.cl_spell_casting_time)
+    CaptionLayout mCLCastingTime;
     @BindView(R.id.tv_spell_range)
     TextView mTVRange;
+    @BindView(R.id.cl_spell_range)
+    CaptionLayout mCLRange;
     @BindView(R.id.tv_spell_target)
     TextView mTVTarget;
+    @BindView(R.id.cl_spell_target)
+    CaptionLayout mCLTarget;
     @BindView(R.id.tv_spell_effect)
     TextView mTVEffect;
+    @BindView(R.id.cl_spell_effect)
+    CaptionLayout mCLEffect;
     @BindView(R.id.tv_spell_duration)
     TextView mTVDuration;
+    @BindView(R.id.cl_spell_duration)
+    CaptionLayout mCLDuration;
     @BindView(R.id.tv_spell_saving_throw)
     TextView mTVSavingThrow;
+    @BindView(R.id.cl_spell_saving_throw)
+    CaptionLayout mCLSavingThrow;
     @BindView(R.id.tv_spell_spell_resistance)
     TextView mTVSpellResistance;
+    @BindView(R.id.cl_spell_spell_resistance)
+    CaptionLayout mCLSpellResistance;
     @BindView(R.id.tv_spell_spell_flavour_text)
     TextView mTVFlavourText;
     @BindView(R.id.tv_spell_description)
@@ -110,20 +140,74 @@ public class SpellDetailFragment extends Fragment implements Browser.DetailView{
 
     @Override
     public void setData(Spell data) {
+        // Name
         mTVName.setText(data.getName());
-        //mTVSchools.setText();
-        //mTVSubschools.setText();
-        //mTVDescriptors.setText();
-        //mTVLevels.setText();
-        //mTVComponents.setText();
-        mTVCastingTime.setText(data.getCastingTime());
-        mTVRange.setText(data.getRange());
-        mTVTarget.setText(data.getTarget());
-        mTVEffect.setText(data.getEffect());
-        mTVDuration.setText(data.getDuration());
-        mTVSavingThrow.setText(data.getSavingThrow());
-        mTVSpellResistance.setText(data.getSpellResistance());
+        // Schools
+        mTVSchools.setText(TextUtils.join(", ", data.getSchools()));
+        // Subschools
+        if (data.getSubschools() != null && !data.getSubschools().isEmpty()) {
+            mTVSubschools.setText("(" + TextUtils.join(", ", data.getSubschools()) + ")");
+        } else {
+            mTVSubschools.setVisibility(View.GONE);
+        }
+        // Descriptors
+        if (data.getDescriptors() != null && !data.getDescriptors().isEmpty()) {
+            mTVDescriptors.setText("[" + TextUtils.join(", ", data.getSubschools()) + "]");
+        } else {
+            mTVDescriptors.setVisibility(View.GONE);
+        }
+        // Levels / Sources
+        if (data.getSources() != null && !data.getSources().isEmpty()) {
+
+            StringBuilder sb;
+            SourceLevelExtraTriplet slet;
+            List<String> sources = new ArrayList<>(data.getSources().size());
+            for (int i = 0; i < data.getSources().size(); i++) {
+                slet = data.getSources().get(i);
+                sb = new StringBuilder();
+                sb.append(slet.getSource()).append(" ").append(String.valueOf(slet.getLevel()));
+                if (!TextUtils.isEmpty(slet.getExtra())) {
+                    sb.append(" (").append(slet.getExtra()).append(")");
+                }
+
+                sources.add(sb.toString());
+            }
+
+            Collections.sort(sources);
+
+            TextView tv;
+            for (String source : sources) {
+                tv = new TextView(getContext());
+                tv.setLayoutParams(mTVLevels.getLayoutParams());
+                tv.setText(source);
+                mContainerLevels.addView(tv);
+                mTVLevels.setVisibility(View.GONE);
+            }
+        } else {
+            mCLLevels.setVisibility(View.GONE);
+        }
+        // Components
+        if (data.getComponents() != null && !data.getComponents().isEmpty()) {
+            mTVComponents.setText(TextUtils.join(", ", data.getComponents()));
+        } else {
+            mTVComponents.setVisibility(View.GONE);
+        }
+        // Casting Time
+        setSectionTextorHide(mCLCastingTime, mTVCastingTime, data.getCastingTime());
+        // Range
+        setSectionTextorHide(mCLRange, mTVRange, data.getRange());
+        // Target
+        setSectionTextorHide(mCLTarget, mTVTarget, data.getTarget());
+        // Effect
+        setSectionTextorHide(mCLEffect, mTVEffect, data.getEffect());
+        // Duration
+        setSectionTextorHide(mCLDuration, mTVDuration, data.getDuration());
+        // Saving Throw
+        setSectionTextorHide(mCLSavingThrow, mTVSavingThrow, data.getSavingThrow());
+        // Spell Resistance
+        setSectionTextorHide(mCLSpellResistance, mTVSpellResistance, data.getSpellResistance());
         //mTVFlavourText.setText();
+        // Description
         mTVDescription.setText(Html.fromHtml(data.getDescription().getFormatted()));
 
         Activity activity = getActivity();
@@ -131,4 +215,25 @@ public class SpellDetailFragment extends Fragment implements Browser.DetailView{
             ((AbsMasterDetailActivity) activity).setActionBarTitle(data.getName());
         }
     }
+
+    private void setSectionTextorHide(CaptionLayout sectionLayout, TextView sectionTextView,
+            CharSequence text) {
+        if (sectionLayout == null) {
+            Log.w(TAG, "setSectionTextorHide: sectionLayout is null");
+            return;
+        }
+        if (sectionTextView == null) {
+            sectionLayout.setVisibility(View.GONE);
+            Log.w(TAG, "setSectionTextorHide: sectionTextView is null");
+            return;
+        }
+
+        if (!TextUtils.isEmpty(text)) {
+            sectionTextView.setText(text);
+            sectionLayout.setVisibility(View.VISIBLE);
+        } else {
+            sectionLayout.setVisibility(View.GONE);
+        }
+    }
+
 }
