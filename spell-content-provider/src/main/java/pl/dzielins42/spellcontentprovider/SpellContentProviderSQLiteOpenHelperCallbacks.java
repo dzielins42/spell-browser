b984f390c984208dfe7658c4dde47235e587e465
Dominik Zieliński <dzielins42@gmail.com>
2017-08-05 19:49:36 +0200

    New spell-content-provider module

---
+++ spell-content-provider/src/main/java/pl/dzielins42/spellcontentprovider/SpellContentProviderSQLiteOpenHelperCallbacks.java
@@ -0,0 +1,6 @@
+package pl.dzielins42.spellcontentprovider;
+
+import pl.dzielins42.spellcontentprovider.base.BaseSQLiteOpenHelperCallbacks;
+
+public class SpellContentProviderSQLiteOpenHelperCallbacks extends BaseSQLiteOpenHelperCallbacks {
+}
