f0ec0963c68ad0b629527f8e28bb59ad489743e8
dzielins42 <dzielins42@gmail.com>
2017-11-03 09:09:31 +0100

    Test Gson export

---
+++ spell-content-provider/src/test/java/pl/dzielins42/spellcontentprovider/spell/composite/GsonExportTest.java
@@ -0,0 +1,93 @@
+package pl.dzielins42.spellcontentprovider.spell.composite;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+
+import pl.dzielins42.spellcontentprovider.characterclass.CharacterClassBean;
+import pl.dzielins42.spellcontentprovider.component.ComponentBean;
+import pl.dzielins42.spellcontentprovider.rulebook.RulebookBean;
+import pl.dzielins42.spellcontentprovider.school.SchoolBean;
+
+import static org.junit.Assert.*;
+
+public class GsonExportTest {
+
+    private Gson mGson;
+
+    @Before
+    public void setUp() throws Exception {
+        mGson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mGson = null;
+    }
+
+    @Test
+    public void exportSingle() throws Exception {
+        CompositeSpellBean bean = CompositeSpellBean.newInstance(
+                0L,
+                "Fireball",
+                RulebookBean.newInstance(0, "5e SRD"),
+                142,
+                "1 action",
+                "150 feet",
+                "#no-target#",
+                "#no-effect#",
+                "20-foot-radius sphere",
+                "instantaneous",
+                "DEX",
+                "NO",
+                "#descriptionPlain#",
+                "#descriptionFormatted#",
+                "#shortDescriptionPlain#",
+                "#shortDescriptionFormatted#",
+                "#flavourTextPlain#",
+                "#flavourTextFormatted#"
+        );
+
+        Set<CharacterClassLevelExtraBean> classesLevels = new HashSet<>();
+        classesLevels.add(CharacterClassLevelExtraBean.newInstance(
+                CharacterClassBean.newInstance(0L, "sorcerer"),
+                null,
+                3
+        ));
+        classesLevels.add(CharacterClassLevelExtraBean.newInstance(
+                CharacterClassBean.newInstance(1L, "wizard"),
+                null,
+                3
+        ));
+        bean.setClassesLevels(classesLevels);
+
+        Set<SchoolSubschoolBean> schools = new HashSet<>();
+        schools.add(SchoolSubschoolBean.newInstance(
+                SchoolBean.newInstance(0L, "evocation"),
+                null
+        ));
+        bean.setSchools(schools);
+
+        Set<ComponentBean> components = new HashSet<>();
+        components.add(ComponentBean.newInstance(0L, "verbal"));
+        components.add(ComponentBean.newInstance(1L, "somatic"));
+        components.add(ComponentBean.newInstance(2L, "material"));
+        bean.setComponents(components);
+
+        bean.setDescriptors(Collections.EMPTY_SET);
+
+        String json = mGson.toJson(bean);
+
+        System.out.println(json);
+    }
+
+}
