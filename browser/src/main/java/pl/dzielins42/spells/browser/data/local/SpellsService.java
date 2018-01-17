d8bc19b2be048d70db8f24b3895f943182f6c314
dzielins42 <dzielins42@gmail.com>
2018-01-17 15:43:26 +0100

    Implement SpellsService stub

--- browser/src/main/java/pl/dzielins42/spells/browser/data/local/SpellsService.java
+++ browser/src/main/java/pl/dzielins42/spells/browser/data/local/SpellsService.java
@@ -1,20 +1,37 @@
 package pl.dzielins42.spells.browser.data.local;
 
-import android.util.Log;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import io.reactivex.Observable;
+import pl.dzielins42.spellcontentprovider.characterclass.CharacterClassBean;
 import pl.dzielins42.spellcontentprovider.characterclass.CharacterClassDao;
+import pl.dzielins42.spellcontentprovider.component.ComponentBean;
 import pl.dzielins42.spellcontentprovider.component.ComponentDao;
+import pl.dzielins42.spellcontentprovider.descriptor.DescriptorBean;
 import pl.dzielins42.spellcontentprovider.descriptor.DescriptorDao;
+import pl.dzielins42.spellcontentprovider.rulebook.RulebookBean;
 import pl.dzielins42.spellcontentprovider.rulebook.RulebookDao;
+import pl.dzielins42.spellcontentprovider.school.SchoolBean;
 import pl.dzielins42.spellcontentprovider.school.SchoolDao;
 import pl.dzielins42.spellcontentprovider.spell.SpellDao;
+import pl.dzielins42.spellcontentprovider.spell.composite.CompositeSpellBean;
+import pl.dzielins42.spellcontentprovider.subschool.SubschoolBean;
 import pl.dzielins42.spellcontentprovider.subschool.SubschoolDao;
 
 @Singleton
 public class SpellsService {
+
+    private final CharacterClassDao mCharacterClassDao;
+    private final ComponentDao mComponentDao;
+    private final DescriptorDao mDescriptorDao;
+    private final RulebookDao mRulebookDao;
+    private final SchoolDao mSchoolDao;
+    private final SpellDao mSpellDao;
+    private final SubschoolDao mSubschoolDao;
+
 
     @Inject
     public SpellsService(
@@ -26,13 +43,41 @@ public class SpellsService {
             SpellDao spellDao,
             SubschoolDao subschoolDao
     ) {
-        Log.d(
-                "dzielins",
-                "SpellsService() called with: characterClassDao = [" + characterClassDao + "], " +
-                        "componentDao = [" + componentDao + "], descriptorDao = [" +
-                        descriptorDao + "], rulebookDao = [" + rulebookDao + "], schoolDao = [" +
-                        schoolDao + "], spellDao = [" + spellDao + "], subschoolDao = [" + subschoolDao + "]"
-        );
+        mCharacterClassDao = characterClassDao;
+        mComponentDao = componentDao;
+        mDescriptorDao = descriptorDao;
+        mRulebookDao = rulebookDao;
+        mSchoolDao = schoolDao;
+        mSpellDao = spellDao;
+        mSubschoolDao = subschoolDao;
+    }
+
+    public Observable<List<CharacterClassBean>> getAllCharacterClasses(){
+        return Observable.never();
+    }
+
+    public Observable<List<ComponentBean>> getAllComponents(){
+        return Observable.never();
+    }
+
+    public Observable<List<DescriptorBean>> getAllDescriptors(){
+        return Observable.never();
+    }
+
+    public Observable<List<RulebookBean>> getAllRulebooks(){
+        return Observable.never();
+    }
+
+    public Observable<List<SchoolBean>> getAllSchools(){
+        return Observable.never();
+    }
+
+    public Observable<List<SubschoolBean>> getAllSubschools(){
+        return Observable.never();
+    }
+
+    public Observable<List<CompositeSpellBean>> getAllSpells(){
+        return Observable.never();
     }
 
 }
